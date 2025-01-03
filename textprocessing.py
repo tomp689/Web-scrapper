89a42c9107b2170bd3cbf626855ab98f708f6840
tomp689 <tomp9838@gmail.com>
2025-01-03 11:41:57 +0200

    Added comments in Greek, removed unnecessary comments and commands

--- textprocessing.py
+++ textprocessing.py
@@ -8,73 +8,23 @@ with open("data.json") as data:
 	articles = data.read()
 
 
-
-# Turning letters to lower case
 def lowercase(articles):
 
-	# Turn all text into lowercase
+	# Μετατροπή των γραμμάτων σε μικρά
 	lowered = articles.lower()
-
-	# print(lowered)
 	
 	return lowered
 
 lowered = lowercase(articles)
 
 
-# Stemming -- not necessary I think
-"""
-def stemming(lowered):
-
-	lowered = lowercase(articles)
-
-	print("BEGIN STEMMING")
-
-	porter = nltk.PorterStemmer()
-	stemmed = [porter.stem(t) for t in articles]
-
-	print(stemmed)
-
-	print("STEMMING COMPLETE")
-
-	return stemmed
-"""
-
-
-
-# Lemmatization -- Also not necessary I think
-
-"""
-Κατέβασε το wordnet: nltk.download('wordnet')
-
-def lemmatization(lowered):
-
-	lemmatizer = nltk.WordNetLemmatizer()
-	lemmantized = [lemmatizer.lemmatize(t) for t in articles]
-
-	print(lemmatized)
-
-	return lemmatized
-
-
-
-lemmatization(articles)
-"""
-
-# stemmed = stemming(articles)
-# OR lemmatized = lemmatization(articles)
-
-
-
 # Tokenization
 def tokenization(lowered):
 
-	# Split the lowered text into tokens and remove all punctuation with NLTK's tokenizer
+	# Χωρίζουμε το κείμενο σε tokens και αφαιρούμε σημεία στίξης και ειδικούς χαρακτήρες με τον tokenizer της NLTK
 	tokenizer = RegexpTokenizer(r'\w+')
 
 	tokenized = tokenizer.tokenize(lowered)
-
-	# print(tokenized)
 
 	return tokenized
 
@@ -82,25 +32,24 @@ def tokenization(lowered):
 tokenized = tokenization(lowered)
 
 
-# Stop words removal
+# Αφαίρεση stop-words
 def remove_stopwords(tokenized):
 
-	# Για τα προθήματα, κατέβασε nltk.download('stopwords')
+   	# Δημιουργία λίστας με όλες τις stop-words της NLTK
 	stopwords = nltk.corpus.stopwords.words('english')
 
 	cleaned_tokens = []
 
-	# If the token is not a stopword, it gets added to the list of cleaned tokens
+	# Αν το token δεν είναι stop-word, τότε θεωρείται "καθαρισμένο"
 	for i in tokenized:
 		if i not in stopwords:
 			cleaned_tokens.append(i)
-
-	# print(cleaned_tokens)
 
 	return cleaned_tokens
 
 
 cleaned_tokens = remove_stopwords(tokenized)
 
+# Όλα τα tokens αποθηκεύονται σε ένα αρχείο
 with open("processed_data.json", "w", encoding = "utf-8") as processed:
 	json.dump(cleaned_tokens, processed, ensure_ascii = False, indent = 4)
